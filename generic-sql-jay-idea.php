64b94e0572ecfbe429bd5baf20d6fdbf149f9cc6
codechaitu <43373747+codechaitu@users.noreply.github.com>
2019-03-29 14:05:05 +0900

    modification after jay's refactoring

--- generic-sql-jay-idea.php
+++ generic-sql-jay-idea.php
@@ -1,7 +1,4 @@
-
-
 <?php
-//Find this code online and run: https://paiza.io/projects/fDRfKeXELNIlI_GB1HxeGw
 
 function main(){
 // 1) Declaration of required variables
@@ -13,30 +10,36 @@ $whereClause = "status = active AND (expiration_date IS NULL OR expiration_date 
 $constraintClause = "force index(user_id)";
 
 // 3) Variables needed for this code
-$sqlQuery = "";
+$sqlQuery = NULL;
 
 // Check if selectClause and whereClause have values and they are valid
-if(!empty($whereClause) && !empty($varSelect)){
-    $constraints = multiexplode(array("AND","OR"), $whereClause);
-    if(count($constraints)>1){
-        echo "There are multiple where clause conditions\n";
-        // Validation needed here
-    }else{
-        echo "There is only one condition, need to validate it.\n";
-        // Validation needed here
-    }
-    
-    // Preparing SQL query
-    if(!empty($constraintClause)){
-        $sqlQuery = $sqlQuery."SELECT ".$varSelect." FROM ACL ".$constraintClause." ".$whereClause;
-    }else{
-        $sqlQuery = $sqlQuery."SELECT ".$varSelect." FROM ACL ".$whereClause;
-    }
-    echo $sqlQuery;
-    
-}else{
+if(empty($whereClause) || empty($varSelect)){
     echo "Where/Select clause is empty; Not accepted";
+    return;
 }
+
+$constraints = multiexplode(array("AND","OR"), $whereClause);
+if(count($constraints)>1){
+    echo "There are multiple where clause conditions\n";
+    // Validation needed here
+}else{
+    echo "There is only one condition, need to validate it.\n";
+    // Validation needed here
+}
+
+// Preparing SQL query
+if(!empty($constraintClause)){
+    $sqlQuery = getSqlFromParams($varSelect, $constraintClause, $whereClause);
+}else{
+    $sqlQuery = getSqlFromParams($varSelect, NULL, $whereClause);
+}
+echo $sqlQuery;
+
+
+}
+
+function getSqlFromParams($varSelect, $constraintClause, $whereClause){
+    return "SELECT ".$varSelect." FROM ACL ".$constraintClause." ".$whereClause;
 }
 
 function multiexplode ($delimiters,$string) {
